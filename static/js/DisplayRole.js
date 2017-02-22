da37aedf981061c483f7214c1102d8e0b48fb29c
jewbre <jewbre18@gmail.com>
2017-02-23 00:13:55 +0100

    image addition as response on enter

--- static/js/DisplayRole.js
+++ static/js/DisplayRole.js
@@ -10,7 +10,13 @@ var DisplayRole = (function () {
         var currentText = displayText.text();
         switch (text) {
             case 'enter':
-                console.log('do some search, maybe?');
+                var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" + currentText + "&api.key=1e6032dcaefa640828c69af54b4877a8&format=json";
+                $.ajax({
+                    dataType: "jsonp",
+                    url: url,
+                    data: {}
+                });
+                currentText = '';
                 break;
             case 'backspace':
                 currentText = currentText.substr(0, Math.max(0, currentText.length - 1));
@@ -22,3 +28,11 @@ var DisplayRole = (function () {
     };
     return DisplayRole;
 }());
+function jsonFlickrApi(rsp) {
+    var output = '';
+    for (var _i = 0, _a = rsp.photos.photo; _i < _a.length; _i++) {
+        var photo = _a[_i];
+        output += "\n    <img src=\"https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg\"/>\n";
+    }
+    $('#photoContainer').html(output);
+}
--- static/ts/DisplayRole.ts
+++ static/ts/DisplayRole.ts
@@ -1,22 +1,27 @@
-
 class DisplayRole {
 
 
     constructor() {
         let self = this;
-        SocketIOSingleton.getInstance().declareAsDisplay(function(letter : string){
+        SocketIOSingleton.getInstance().declareAsDisplay(function (letter: string) {
             self.displayText(letter);
         });
     }
 
-    private displayText(text : string) {
+    private displayText(text: string) {
         let displayText = $('#display-text');
 
         let currentText = displayText.text();
 
         switch (text) {
             case 'enter' :
-                console.log('do some search, maybe?');
+                let url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=${currentText}&api.key=1e6032dcaefa640828c69af54b4877a8&format=json`;
+                $.ajax({
+                    dataType: "jsonp",
+                    url: url,
+                    data: {}
+                });
+                currentText = '';
                 break;
             case 'backspace':
                 currentText = currentText.substr(0, Math.max(0, currentText.length - 1));
@@ -28,5 +33,34 @@ class DisplayRole {
         displayText.text(
             currentText
         );
+    }
+}
+
+function jsonFlickrApi(rsp: FlickApiResponse) {
+    let output : string = '';
+    for(let photo of rsp.photos.photo) {
+        output += `
+    <img src="https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg"/>
+`;
+    }
+
+    $('#photoContainer').html(output);
+}
+
+interface FlickApiResponse {
+    stat: string;
+    photos: {
+        photo: {
+            farm: number
+            id: string
+            isfamily: number
+            isfriend: number
+            ispublic: number
+            owner: string
+            secret: string
+            server: string
+            title: string
+
+        }[]
     }
 }
